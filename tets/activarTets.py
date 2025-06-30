c5890ebca28c0f6870ead7f6f304ccb69e0e0be7
David-Peralta-Rd <crdavidperalta@gmail.com>
2025-06-30 15:49:27 -0500

    Subiendo carpeta tests y coverage.

---
+++ tets/activarTets.py
@@ -0,0 +1,11 @@
+from cart import tests as testCart
+from orders import tests as testOrders
+from products import tests as testProduct
+
+# Invocar test activarlos
+
+testCart()
+testOrders()
+testProduct()
+
+print("Tets echo")
